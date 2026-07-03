29f2a06f84dd805e9541468e0f0304181a3af634
vamcart <vamshop@gmail.com>
2026-07-03 09:34:17 +0300

    Добавлено  user_id поле при создании ссылки, записывается id юзера

--- src/app/Models/Link.php
+++ src/app/Models/Link.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Link extends Model
 {
     protected $fillable = ['url','short_url', 'short_url_hash'];
+
+    protected static function booted() {
+        static::creating(function($model) {
+            $model->user_id = Auth::user()->id;
+        });
+    }
 
     public function visits(): HasMany
     {
